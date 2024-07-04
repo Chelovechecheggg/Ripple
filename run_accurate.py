3c2c4d2464c9d8ed4ed847a11c655ccf66d3334a
Chelovechecheggg <xela.xela231@gmail.com>
2024-07-04 12:52:31 +0300

    start files change for globus3

--- run_accurate.py
+++ run_accurate.py
@@ -1,19 +1,22 @@
 import ripple_main as ripperoni
 
-N = 35
+N = 5
 Vol_size = [400, 400, 360]
 Vol_start = [-200, -200, -180]
-coil_res = 5
-vol_res = 5
+coil_res = 0.5
+vol_res = 0.5
 # plane_Xminmax = [10, 180]
 # plane_Zminmax = [-150, 150]
+#planes_Xminmax = [[1, 20], [20, 40], [40, 60], [60, 90]]
+#planes_Zminmax = [[-100, -90], [-90, -70], [-70, -50], [-50, -30], [-30, -10], [-10, 10],
+#                  [10, 30], [30, 50], [50, 70], [70, 90], [90, 100]]
 planes_Xminmax = [[10, 30], [30, 50], [50, 70], [70, 90], [90, 110], [110, 130], [130, 150], [150, 170], [170, 180]]
 planes_Zminmax = [[-150, -130], [-130, -110], [-110, -90], [-90, -70], [-70, -50], [-50, -30], [-30, -10], [-10, 10],
                   [10, 30], [30, 50], [50, 70], [70, 90], [90, 110], [110, 130], [130, 150]]
 plane_max_angle = 22.5
 plane_min_angle = 11.25
 only_read = True
-output_filename = f'Globus3_coils/Ripple_curv_upd/Ripple_fill_curv_upd_10.txt'
+output_filename = f'Globus3_coils/Ripple_curv_upd/Ripple_fill_5.txt'
 
 ripperoni.approx_N_coils('Globus3_coils', f"Globus3_coils/coils_base_curved_upd.txt", N, 4)
 ripperoni.clone_coils('Globus3_coils', N, 16)
